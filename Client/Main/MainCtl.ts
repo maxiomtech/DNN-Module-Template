3caf0c73138e085e8fd78637f94c78f840dffbc5
Jonathan Sheely <jsheely@twentytech.net>
2014-10-27 23:43:07 -0400

    Added base example controller and updated DNN manifest

---
+++ Client/Main/MainCtl.ts
@@ -0,0 +1,29 @@
+﻿ module CompanyName.ModuleName {
+     
+     interface IMainCtl {
+         init(): void;
+     }
+
+     
+
+     class MainCtl implements IMainCtl {
+         
+         hello: string;    
+
+         constructor(private $scope) {
+             var vm = this;
+
+             this.init();
+             
+         }
+
+         public init() {
+             this.hello = "Hello World";
+         }
+
+         
+     }
+
+     app.controller('MainCtl', ['$scope', MainCtl]);
+
+ }
--- Views/Main.ascx
+++ Views/Main.ascx
@@ -1,1 +1,5 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Main.ascx.cs" Inherits="CompanyName.ModuleName.Views.Main" %>
+
+<div ng-controller="vm as MainCtl">
+    {{vm.Hello}}
+</div>
