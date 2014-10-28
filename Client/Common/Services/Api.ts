61e76dd65bc09baf5894309df696e523dd4250c8
Jonathan Sheely <jsheely@twentytech.net>
2014-10-27 23:22:11 -0400

    Added Api service and updated autofac reference.

--- Client/Common/Services/Api.ts
+++ Client/Common/Services/Api.ts
@@ -1,3 +1,39 @@
-﻿ module CompanyName {
-     
- }
+﻿module CompanyName.ModuleName {
+    
+    export interface IApi {
+        getData: () => ng.IHttpPromise<any>;
+        getDataRx: () => Rx.IObservable<ng.IHttpPromise<any>>;
+    }
+
+
+    class Api implements  IApi {
+        constructor(private $http: ng.IHttpService, private rx, private $log: ng.ILogService) { }
+
+        private httpConfig = {
+            headers: {
+                TabId: servicesFramework.getTabId(),
+                ModuleId: servicesFramework.getModuleId(),
+                RequestVerificationToken: servicesFramework.getAntiForgeryValue(),
+            }
+        }
+
+        //Example of getting data from WebApi
+        public getData() {
+            return this.$http.get(servicesFramework.getServiceRoot('CompanyName/ModuleName') + 'ModuleName/', this.httpConfig);
+        }
+
+        //Example of returning an Observable Stream of Promise
+        public getDataRx() {
+            return this.rx.Observable.fromPromise(
+                this.getData()
+            );
+        }
+
+
+    }
+
+    ModuleName.app.service('Api', ['$http', 'rx', '$log', Api]);
+
+}
+
+
