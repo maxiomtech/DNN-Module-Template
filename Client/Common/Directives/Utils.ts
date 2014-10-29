1957f46d66075458e4c320ba49e39769a9d0b670
Jonathan Sheely <jsheely@twentytech.net>
2014-10-28 23:53:34 -0400

    Added example filter and example directive

---
+++ Client/Common/Directives/Utils.ts
@@ -0,0 +1,26 @@
+﻿module CompanyName.ModuleName {
+
+    interface INgEnter extends  ng.IScope {
+        ngEnter(): void;
+    }
+
+
+    function ngEnter(): ng.IDirective {
+         return {
+            restrict: 'A',
+            scope: { ngEnter: '&' },
+            link: (scope : INgEnter, element, attributes) => {
+                element.bind("keydown keypress", event => {
+                    if (event.which === 13) {
+                        event.preventDefault();
+                        scope.ngEnter();
+                    }
+                });
+            }
+        }
+     }
+
+
+    app.directive('ngEnter', ngEnter);
+
+}
