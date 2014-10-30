1776341debaf9f8933d9989df99b973eb40d01fe
Jonathan Sheely <jsheely@twentytech.net>
2014-10-29 20:44:09 -0400

    Added ngEnter, noIdTabs and dateFormatter directives

--- Client/Common/Directives/Utils.ts
+++ Client/Common/Directives/Utils.ts
@@ -1,27 +1,55 @@
 ﻿module CompanyName.ModuleName {
 
-    interface INgGreeting extends  ng.IScope {
-        ngEnter(): void;
-    }
-
-    //Mimics the ngEnter directive functionality.
-    //Remember that camelCase is used like ng-greeting in templates.
-    function ngGreeting(): ng.IDirective {
-         return {
+    function ngEnter(): ng.IDirective {
+        return {
             restrict: 'A',
-            scope: { ngEnter: '&' },
-             link: (scope: INgGreeting, element, attributes) => {
+            scope: {
+                ngEnter: '&'
+            },
+            link: (scope: any, element, attrs) => {
                 element.bind("keydown keypress", event => {
                     if (event.which === 13) {
                         event.preventDefault();
-                        scope.ngEnter();
+                        scope.$apply(scope.ngEnter());
                     }
                 });
             }
         }
-     }
+    }
+
+    function noIdTabs(): ng.IDirective {
+        return {
+            restrict: 'A',
+            link: (scope, element: any, attrs) => {
+                var rand = 'spec' + (new Date()).getTime() + Math.floor((Math.random() * 25) + 65);
+                element.find('ul.nav li a').each((index, o) => {
+                    angular.element(o).attr("href", "#" + rand + index.toString());
+                });
+
+                element.find('div').each((index, o) => {
+                    angular.element(o).attr("id", rand + index.toString());
+                });
+
+                element.tabs();
+            }
+        }
+    }
+
+    function dateFormatter(): ng.IDirective {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: (scope, element, attrs, ngModel) => {
+                ngModel.$formatters.push((value) => {
+                    return moment(value).startOf('day').toDate();
+                });
+            }
+        }
+    }
 
 
-    app.directive('ngGreeting', ngGreeting);
+    app.directive('dateFormatter', dateFormatter)
+        .directive('ngEnter', ngEnter)
+        .directive('noIdTabs', noIdTabs);
 
 }
