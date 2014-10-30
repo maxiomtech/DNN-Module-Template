1f4105e5373fcfe007e82a0589f08b19a332e4e4
Jonathan Sheely <jsheely@twentytech.net>
2014-10-29 20:43:31 -0400

    Revert "Update utils"
    This reverts commit 263123dc420d5bc26b561a47eab85cfcec1fac2d.

--- Client/Common/Directives/Utils.ts
+++ Client/Common/Directives/Utils.ts
@@ -1,55 +1,27 @@
 ﻿module CompanyName.ModuleName {
 
-    function ngEnter(): ng.IDirective {
-        return {
+    interface INgGreeting extends  ng.IScope {
+        ngEnter(): void;
+    }
+
+    //Mimics the ngEnter directive functionality.
+    //Remember that camelCase is used like ng-greeting in templates.
+    function ngGreeting(): ng.IDirective {
+         return {
             restrict: 'A',
-            scope: {
-                ngEnter: '&'
-            },
-            link: (scope: any, element, attrs) => {
+            scope: { ngEnter: '&' },
+             link: (scope: INgGreeting, element, attributes) => {
                 element.bind("keydown keypress", event => {
                     if (event.which === 13) {
                         event.preventDefault();
-                        scope.$apply(scope.ngEnter());
+                        scope.ngEnter();
                     }
                 });
             }
         }
-    }
-
-    function noIdTabs(): ng.IDirective {
-        return {
-            restrict: 'A',
-            link: (scope, element: any, attrs) => {
-                var rand = 'spec' + (new Date()).getTime() + Math.floor((Math.random() * 25) + 65);
-                element.find('ul.nav li a').each((index, o) => {
-                    angular.element(o).attr("href", "#" + rand + index.toString());
-                });
-
-                element.find('div').each((index, o) => {
-                    angular.element(o).attr("id", rand + index.toString());
-                });
-
-                element.tabs();
-            }
-        }
-    }
-
-    function dateFormatter(): ng.IDirective {
-        return {
-            restrict: 'A',
-            require: 'ngModel',
-            link: (scope, element, attrs, ngModel) => {
-                ngModel.$formatters.push((value) => {
-                    return moment(value).startOf('day').toDate();
-                });
-            }
-        }
-    }
+     }
 
 
-    app.directive('dateFormatter', dateFormatter)
-        .directive('ngEnter', ngEnter)
-        .directive('noIdTabs', noIdTabs);
+    app.directive('ngGreeting', ngGreeting);
 
 }
